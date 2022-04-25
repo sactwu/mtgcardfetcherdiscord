0976aae9adc3a51e91ea23b6e56b0dbdbf4f6889
sactwu <lukian@sapo.pt>
2022-04-25 22:14:37 +0200

    reorder text output

--- scryfall.py
+++ scryfall.py
@@ -45,21 +45,22 @@ def get_all_cards_values(card, extras):
     return cards_values
 
 
-def get_card_values(card, link=true):
+def get_card_values(card, link=True):
     values = dict()
     values['Name'] = card.name()
     values['Cost'] = card.mana_cost()
     values['Type'] = card.type_line()
     values['Text'] = card.oracle_text()
-    if link:
-        values['Scryfall Link'] = card.scryfall_uri()
     if 'Creature' in values['Type']:
         values['Power/Toughness'] = f'{card.power()}/{card.toughness()}'
     if 'Planeswalker' in values['Type']:
         values['Loyalty'] = card.loyalty()
+    values['Rarity'] = card.rarity()
     price = card.prices('eur')
     if price:
         values['Price'] = price + '€'
+    if link:
+        values['Scryfall Link'] = card.scryfall_uri()
     values['Image'] = card.image_uris(image_type='normal')
     return values
 
@@ -69,7 +70,7 @@ def get_part_values(part):
     values['Name'] = part['name']
     values['Type'] = part['type_line']
     if part['object'] == 'related_card':
-        values = get_card_values(scrython.cards.Id(id=part['id']), link=false)
+        values = get_card_values(scrython.cards.Id(id=part['id']), link=False)
     else:
         print(part)
         values['Cost'] = part['mana_cost']
